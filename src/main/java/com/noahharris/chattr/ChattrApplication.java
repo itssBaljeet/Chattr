a47bae35b45f47f1e8fb2990da9611349f4ddc0e
itssBaljeet <noah.kendallh@gmail.com>
2024-08-28 17:54:59 -0400

    first commit

---
+++ src/main/java/com/noahharris/chattr/ChattrApplication.java
@@ -0,0 +1,13 @@
+package com.noahharris.chattr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noahharris/chattr/ChattrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noahharris.chattr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
