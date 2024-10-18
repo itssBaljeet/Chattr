570f561519e696f91c70f904f74647493d471359
itssBaljeet <noah.kendallh@gmail.com>
2024-10-18 11:21:58 -0400

    updated project to use jakarta EE instead of javax

--- src/main/java/com/noahharris/chattr/repository/UserRepository.java
+++ src/main/java/com/noahharris/chattr/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.noahharris.chattr.repository;
 
 import com.noahharris.chattr.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
