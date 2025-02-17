af33495f2c30e660b35f7e635cb4ca8f111bc1c4
itssBaljeet <noah.kendallh@gmail.com>
2025-02-16 23:43:34 -0500

    refactored setUpPeerConnection and setUpMedia into startCall

--- src/main/resources/static/js/videoCall.js
+++ src/main/resources/static/js/videoCall.js
@@ -7,20 +7,23 @@ const chatInput = document.getElementById('chatInput');
 const sendButton = document.getElementById('sendButton');
 const chatMessages = document.getElementById('chatMessages');
 
-let roomCode = null;
-let currentUsername = '';
-fetchUsername().then(username => {
-    currentUsername = username;
-    console.log("Current Username:", currentUsername); // This will log the string value
-}).catch(error => {
-    console.error("Error setting currentUsername:", error);
-});
-let localStream;
-let peerConnection;
-let stompClient = null;
+// State Object to manage variables
+const callState = {
+    roomCode: null,
+    currentUsername: '',
+    localStream: null,
+    peerConnection: null,
+    stompClient: null
+};
 
-document.getElementById('startMediaButton').addEventListener('click', setupMedia);
-document.getElementById('initiateCallButton').addEventListener('click', setupPeerConnection);
+async function init() {
+    callState.currentUsername = await fetchUsername();
+    console.log("Current Username:", callState.currentUsername);
+}
+
+init();
+
+document.getElementById('initiateCallButton').addEventListener('click', startCall);
 document.getElementById('leaveRoomButton').addEventListener('click', leaveRoom);
 document.getElementById('sendOffer').addEventListener('click', sendOffer);
 
@@ -43,10 +46,10 @@ roomForm.addEventListener('submit', function(event) {
 
 
     if (inputRoomCode) {
-        roomCode = inputRoomCode; // Store the room code
+        callState.roomCode = inputRoomCode; // Store the room code
         document.getElementById("leaveRoomButton").disabled = false; // Enable the WebSocket button
-        console.log("Room code set:", roomCode);
-        setupWebSocket(roomCode);
+        console.log("Room code set:", callState.roomCode);
+        setupWebSocket(callState.roomCode);
     } else {
         alert("Please enter a room code before joining.");
     }
@@ -54,18 +57,18 @@ roomForm.addEventListener('submit', function(event) {
 
 // Shutdown peer connection, websockets, and streams
 function leaveRoom() {
-    if (stompClient && stompClient.connected) {
-        stompClient.disconnect(() => {
+    if (callState.stompClient && callState.stompClient.connected) {
+        callState.stompClient.disconnect(() => {
             console.log('Disconnected from WebSocket server.');
             sendLeaveMessage();
         });
     }
-    if (peerConnection) {
-        peerConnection.close();
-        peerConnection = null;
+    if (callState.peerConnection) {
+        callState.peerConnection.close();
+        callState.peerConnection = null;
         console.log('Peer connection closed.');
     }
-    localStream.getTracks().forEach(track => track.stop());
+    callState.localStream.getTracks().forEach(track => track.stop());
     localVideo.srcObject = null;
     remoteVideo.srcObject = null;
 }
@@ -81,76 +84,67 @@ async function fetchUsername() {
     }
 }
 
-// Get media stream (camera and microphone)
-async function setupMedia() {
-    try {
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        localVideo.srcObject = localStream;
-        document.getElementById("initiateCallButton").disabled = false;
-        console.log('Local stream initialized');
-    } catch (error) {
-        console.error('Error accessing media devices:', error);
-        throw error;
-    }
-}
+
 
 // Set up WebSocket connection and subscriptions
 function setupWebSocket(roomCode) {
     return new Promise((resolve, reject) => {
-        if (stompClient && stompClient.connected) {
+        if (callState.stompClient && callState.stompClient.connected) {
             console.log("WebSocket is already connected.");
             return;
         }
 
         const socket = new SockJS('/ws');
-        stompClient = Stomp.over(socket);
+        callState.stompClient = Stomp.over(socket);
 
-        stompClient.connect({}, function () {
+        callState.stompClient.connect({}, function () {
             console.log('Connected to signaling server');
-            stompClient.subscribe(`/topic/call/${roomCode}`, message => handleSignalingData(JSON.parse(message.body)));
-            stompClient.subscribe(`/topic/chat/${roomCode}`, message => displayChatMessage(JSON.parse(message.body)));
+            callState.stompClient.subscribe(`/topic/call/${roomCode}`, message => handleSignalingData(JSON.parse(message.body)));
+            callState.stompClient.subscribe(`/topic/chat/${roomCode}`, message => displayChatMessage(JSON.parse(message.body)));
             sendJoinMessage();
             resolve();
         }, reject);
     });
 }
 
-// Set up WebRTC peer connection and event handlers
-function setupPeerConnection() {
-    console.log("WebRTC Connection Created")
-    peerConnection = new RTCPeerConnection(configuration);
+async function startCall() {
+    try {
+        console.log("Starting media and webRTC");
+        callState.localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        localVideo.srcObject = callState.localStream
 
-    // Add local stream to peer connection
-    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+        callState.peerConnection = new RTCPeerConnection(configuration)
 
-    // Event: ICE candidate generation
-    peerConnection.onicecandidate = event => {
-        if (event.candidate) sendSignalingData({ type: 'candidate', candidate: event.candidate, sender: currentUsername });
-    };
+        callState.localStream.getTracks().forEach(track =>
+            callState.peerConnection.addTrack(track, callState.localStream)
+        );
 
-    document.getElementById("sendOffer").disabled = false;
+        callState.peerConnection.ontrack = (event) => {
+            remoteVideo.srcObject = event.streams[0];
+        };
 
-    // Event: Remote stream received
-    peerConnection.ontrack = event => {
-        remoteVideo.srcObject = event.streams[0];
-        console.log('Remote stream received');
-    };
+        document.getElementById("sendOffer").disabled = false;
+
+        console.log("Call initialized");
+    } catch (error) {
+        console.error("Error starting call:", error);
+    }
 }
 
 // Send sdp offer to server
 function sendOffer() {
     // Create and send offer
     console.log("Sending offer...")
-    peerConnection.createOffer()
-        .then(offer => peerConnection.setLocalDescription(offer))
-        .then(() => sendSignalingData({ type: 'offer', sdp: peerConnection.localDescription.sdp, sender: currentUsername, roomCode: roomCode }))
+    callState.peerConnection.createOffer()
+        .then(offer => callState.peerConnection.setLocalDescription(offer))
+        .then(() => sendSignalingData({ type: 'offer', sdp: callState.peerConnection.localDescription.sdp, sender: currentUsername, roomCode: callState.roomCode }))
         .catch(error => console.error('Error creating offer:', error));
 }
 
 // Send join message after establishing WebSocket and username
 function sendJoinMessage() {
     sendChatMessage({
-        sender: currentUsername,
+        sender: callState.currentUsername,
         type: 'JOIN'
     });
 }
@@ -158,7 +152,7 @@ function sendJoinMessage() {
 // Send join message after establishing WebSocket and username
 function sendLeaveMessage() {
     sendChatMessage({
-        sender: currentUsername,
+        sender: callState.currentUsername,
         type: 'LEAVE'
     });
 }
@@ -168,21 +162,21 @@ function handleSignalingData(data) {
     console.log("---SENDER---" + data.sender)
     switch (data.type) {
         case 'offer':
-            if (data.sender === currentUsername) {
+            if (data.sender === callState.currentUsername) {
                 console.log("refusing same offer")
                 return;
             }
             handleOffer(data.sdp);
             break;
         case 'answer':
-            if (data.sender === currentUsername) {
+            if (data.sender === callState.currentUsername) {
                 console.log("refusing same answer")
                 return;
             }
             handleAnswer(data.sdp);
             break;
         case 'candidate':
-            if (data.sender === currentUsername) {
+            if (data.sender ===callState.currentUsername) {
                 console.log("refusing same candidate")
                 return;
             }
@@ -198,17 +192,17 @@ function handleSignalingData(data) {
 
 // Handle incoming WebRTC offer
 function handleOffer(offer) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'offer', sdp: offer }))
-        .then(() => peerConnection.createAnswer())
-        .then(answer => peerConnection.setLocalDescription(answer))
-        .then(() => sendSignalingData({ type: 'answer', sdp: peerConnection.localDescription.sdp, roomCode: roomCode, sender: currentUsername }))
+    callState.peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'offer', sdp: offer }))
+        .then(() => callState.peerConnection.createAnswer())
+        .then(answer => callState.peerConnection.setLocalDescription(answer))
+        .then(() => sendSignalingData({ type: 'answer', sdp: callState.peerConnection.localDescription.sdp, roomCode: callState.roomCode, sender: callState.currentUsername }))
         .catch(error => console.error('Error handling offer:', error));
 }
 
 // Handle WebRTC answer from the other peer
 function handleAnswer(answer) {
-    if (!peerConnection.remoteDescription) { // Ensure remote description is not already set
-        peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'answer', sdp: answer }))
+    if (!callState.peerConnection.remoteDescription) { // Ensure remote description is not already set
+        callState.peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'answer', sdp: answer }))
             .catch(error => console.error('Error setting remote description:', error));
     }
 }
@@ -216,13 +210,13 @@ function handleAnswer(answer) {
 
 // Handle incoming ICE candidate
 function handleCandidate(candidate) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+    callState.peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
         .catch(error => console.error('Error adding ICE candidate:', error));
 }
 
 // Send signaling data through WebSocket
 function sendSignalingData(data) {
-    stompClient.send(`/app/signal/${roomCode}`, {}, JSON.stringify(data));
+    callState.stompClient.send(`/app/signal/${callState.roomCode}`, {}, JSON.stringify(data));
 }
 
 // Chat Message Functions
@@ -239,7 +233,7 @@ function sendChat() {
     if (message) {
         sendChatMessage({
             type: 'CHAT',
-            sender: currentUsername,
+            sender: callState.currentUsername,
             content: message
         });
         chatInput.value = '';
@@ -248,7 +242,7 @@ function sendChat() {
 
 // Send chat message through WebSocket
 function sendChatMessage(data) {
-    stompClient.send(`/app/chat/${roomCode}/sendMessage`, {}, JSON.stringify(data));
+    callState.stompClient.send(`/app/chat/${callState.roomCode}/sendMessage`, {}, JSON.stringify(data));
 }
 
 // Display incoming chat messages
